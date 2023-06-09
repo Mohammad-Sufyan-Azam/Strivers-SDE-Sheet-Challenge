86f3a37ef41348e6215e1a77e5442798cb5759d3
Mohammad-Sufyan-Azam <sufyan20312@iiitd.ac.in>
2023-06-09 18:21:44 +0530

    Added q10

---
+++ 10. find duplicates in an array.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &arr, int n){
+	// Write your code here.
+	// hare and tortoise
+	int slow = arr[0], fast = arr[0];
+	while(true) {
+		slow = arr[slow];
+		fast = arr[arr[fast]];
+		if (slow == fast)
+			break;
+	} 
+	fast = arr[0];
+	while(fast != slow) {
+		fast = arr[fast];
+		slow = arr[slow];
+	}
+	return fast;
+}
